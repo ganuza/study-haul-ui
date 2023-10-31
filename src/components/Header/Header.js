6be5c7a43b77087917e8ffe7d6682fcdf10ec70a
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-31 17:55:05 -0600

    Add logo to header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,10 @@
-// import StudyHaul_logo from '../Assets/StudyHaul_logo.png'       
+import logo from '../../Assets/StudyHaul_logo.png'       
 import './Header.css';
 
 function Header() {
   return (
     <div className="header">
-      <img src='StudyHaul_logo.png'/>
+      <img src={logo}/>
       <h1 className='sr-only'>Study Hall</h1>
     </div>
   );
