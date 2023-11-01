54031940c27ae0c8e54260967f4e417f1c4fdcc9
Ann Hochworter <ann.hochworter@gmail.com>
2023-10-31 20:11:18 -0600

    Add styling to main view

--- src/components/AllQuestionsContainer/AllQuestionsContainer.js
+++ src/components/AllQuestionsContainer/AllQuestionsContainer.js
@@ -1,25 +1,31 @@
 import './AllQuestionsContainer.css';
 import AllQuestionsCard from '../AllQuestionsCard/AllQuestionsCard';
+import { NavLink } from 'react-router-dom';
 
-function AllQuestionsContainer({questions}) {
+function AllQuestionsContainer({ questions }) {
+  console.log('AQCont quests: ', questions);
 
-  console.log('AQCont quests: ', questions)
-
-  const allQuestionsCards = questions.map((question) => {
-    return <AllQuestionsCard
-      id={question.id}
-      key={question.id}
-      mod_num={question.mod_num}
-      topic={question.topic}
-      question={question.question}
-      answer={question.answer}
-    />
-  })
+  const allQuestionsCards = questions.map(question => {
+    return (
+      <AllQuestionsCard
+        id={question.id}
+        key={question.id}
+        mod_num={question.mod_num}
+        topic={question.topic}
+        question={question.question}
+        answer={question.answer}
+      />
+    );
+  });
 
   return (
     <div className="all-questions-container">
+      <div className="btn-container">
+        <button className="filter-btn">All Questions</button>
+        <button className="filter-btn">Mod 2</button>
+        <button className="filter-btn">Mod 3</button>
+      </div>
       {allQuestionsCards}
-      <AllQuestionsCard />
     </div>
   );
 }
