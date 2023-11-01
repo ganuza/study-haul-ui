edee86f48aea13fcf8cbe5c8007928881fb7069e
Adrian Zabolitzki <adrianzab@gmail.com>
2023-10-31 18:42:53 -0600

    Render questions cards using props

--- src/components/AllQuestionsCard/AllQuestionsCard.js
+++ src/components/AllQuestionsCard/AllQuestionsCard.js
@@ -1,9 +1,10 @@
 import './AllQuestionsCard.css';
 
-function AllQuestionsCard() {
+function AllQuestionsCard({ id, mod_num, topic, question, answer }) {
   return (
     <div className="all-questions-card">
-      <h2>ALL QUESTIONS CARDS!</h2>
+      <h2>{question}</h2>
+
     </div>
   );
 }
