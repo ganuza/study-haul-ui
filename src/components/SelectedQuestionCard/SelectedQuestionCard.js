b37af708df7f2c5cb2ff8ccc5a5c42724f86cf30
Ann Hochworter <ann.hochworter@gmail.com>
2023-11-02 18:01:16 -0600

    Render question elements to the selected question view

--- src/components/SelectedQuestionCard/SelectedQuestionCard.js
+++ src/components/SelectedQuestionCard/SelectedQuestionCard.js
@@ -1,7 +1,13 @@
 import './SelectedQuestionCard.css';
 
-function SelectedQuestionCard() {
-  return <p>Hi from SelQuestCard!</p>;
+function SelectedQuestionCard({ topic, question, answer }) {
+  return (
+    <div>
+      <h2>{topic}</h2>
+      <p>{question}</p>
+      <p>{answer}</p>
+    </div>
+  );
 }
 
 export default SelectedQuestionCard;
--- src/components/SelectedQuestionContainer/SelectedQuestionContainer.js
+++ src/components/SelectedQuestionContainer/SelectedQuestionContainer.js
@@ -2,16 +2,23 @@ import './SelectedQuestionContainer.css';
 import { getSelectedQuestion } from '../../apiCalls';
 import { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import SelectedQuestionCard from '../SelectedQuestionCard/SelectedQuestionCard';
 
 function SelectedQuestionContainer() {
   const [selectedQuestion, setSelectedQuestion] = useState({});
   const [selectedQuestionError, setSelectedQuestionError] = useState('');
   const { id } = useParams();
-
+  console.log(selectedQuestion);
   getSelectedQuestion(id)
-    .then(data => console.log(data))
+    .then(data => setSelectedQuestion(data))
     .catch(error => setSelectedQuestionError());
-  return <p>Hi from Selected Quest Cont</p>;
+  return (
+    <SelectedQuestionCard
+      topic={selectedQuestion.topic}
+      question={selectedQuestion.question}
+      answer={selectedQuestion.answer}
+    />
+  );
 }
 
 export default SelectedQuestionContainer;
